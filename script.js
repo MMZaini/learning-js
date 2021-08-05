8f63c9a177112af68eee59b26d9397434f197694
Mahdi Zaini <73606041+MMZaini@users.noreply.github.com>
2021-08-05 14:04:18 +0000

    remove powerful click effects temporarily

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let clicks = 1
-let amo1 = 0
+let clicks = 0
 let clickamo = 1
+let amo1 = 0
+let clickcur = 0
 
 function text() {
   let line = "You have " + clicks + " clicks"
+  clickcur = clicks
   clicks = clicks + clickamo
   document.getElementById("clickTime").innerHTML = line
 }
@@ -11,23 +13,26 @@ function text() {
 function shopToggle() {
   var x = document.getElementById("shopText");
   if (x.innerHTML === "") {
-    x.innerHTML = "1. Powerful Clicks - Price = 10 Clicks - Increases purchasing power by 1";
+    x.innerHTML = "1. Powerful Clicks - Price = 15 Clicks - Increases purchasing power by 1";
   } else {
     x.innerHTML = "";
   }
 }
 
 function buy1() {
-  if (clicks < 15 || clicks == 0) {
-   console.log("User does not have enough clicks!")
+  if (clickcur < 15) {
+   console.log("User does not have enough clicks! He has " + clickcur + " Clicks")
   }
-  if (clicks >= 15) {
-   clicks = clicks - 15;
-   line = "You have " + clicks + " clicks"
+
+
+  if (clickcur >= 15) {
+   clickcur = clicks - 16
+   clicks = clicks - 16
+   line = "You have " + clickcur + " clicks"
+   // clickamo = clickamo + 1
    amo1 = amo1 + 1
    document.getElementById("clickTime").innerHTML = line
-   document.getElementById("up1").innerHTML = "You have " + amo1 + " Powerful Clicks Upgrades"
-   clickamo = clickamo + 1
+   document.getElementById("up1").innerHTML = "You have " + amo1 + " 'Powerful Clicks' Upgrades"
    }
 
 }
