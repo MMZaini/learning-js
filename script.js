ed3dc520a1d14aaf303854d116aad1ce563b8405
Mahdi Zaini <73606041+MMZaini@users.noreply.github.com>
2021-08-05 13:11:55 +0000

    add shop and change UI

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-let clicks = 0
-
+let clicks = 1
+let amo1 = 0
+let clickamo = 1
 
 function text() {
   let line = "You have " + clicks + " clicks"
-  document.getElementById("ctbut").innerHTML = line
+  clicks = clicks + clickamo
   document.getElementById("clickTime").innerHTML = line
-  clicks = clicks + 1
 }
 
+function shopToggle() {
+  var x = document.getElementById("shopText");
+  if (x.innerHTML === "") {
+    x.innerHTML = "1. Powerful Clicks - Price = 10 Clicks - Increases purchasing power by 1";
+  } else {
+    x.innerHTML = "";
+  }
+}
+
+function buy1() {
+  if (clicks < 15 || clicks == 0) {
+   console.log("User does not have enough clicks!")
+  }
+  if (clicks >= 15) {
+   clicks = clicks - 15;
+   line = "You have " + clicks + " clicks"
+   amo1 = amo1 + 1
+   document.getElementById("clickTime").innerHTML = line
+   document.getElementById("up1").innerHTML = "You have " + amo1 + " Powerful Clicks Upgrades"
+   clickamo = clickamo + 1
+   }
+
+}
