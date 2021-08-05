e9b4c2b7d1c29e2ce3952e4b6a5a8bce3a194230
Mahdi Zaini <73606041+MMZaini@users.noreply.github.com>
2021-08-05 16:12:56 +0000

    dynamic upgrade prices

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let clicks = 0
 let clickamo = 1
 let amo1 = 0
 
+let price1 = 15
+let multiplier1 = price1 / 4 + price1
+price1 = multiplier1
+
 function text() {
   clicks = clicks + clickamo
   let line = "You have " + clicks + " clicks"
@@ -11,22 +15,26 @@ function text() {
 function shopToggle() {
   var x = document.getElementById("shopText");
   if (x.innerHTML === "") {
-    x.innerHTML = "1. Powerful Clicks - Price = 15 Clicks - Increases purchasing power by 1";
+    x.innerHTML = "1. Powerful Clicks - Price = " + price1 + " - Increases purchasing power by 1";
   } else {
     x.innerHTML = "";
   }
 }
 
 function buy1() {
-  if (clicks < 15) {
-    console.log("User does not have enough clicks! He has " + clicks + " Clicks")
+  if (clicks < price1) {
+    console.log("User does not have enough clicks! He has " + clicks + " clicks! He needs " + price1 + "clicks! He needs " + price1 - clicks + "more clicks!")
   } else {
-    clicks = clicks - 15
+    clicks = clicks - price1
     line = "You have " + clicks + " clicks"
+    multiplier1 = price1 / 4 + price1 
+    price1 = multiplier1
     amo1 = amo1 + 1
     clickamo = clickamo + 1
     document.getElementById("clickTime").innerHTML = line
     document.getElementById("up1").innerHTML = "You have " + amo1 + " 'Powerful Clicks' Upgrades"
+
+
   }
 
 }
