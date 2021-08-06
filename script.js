5f1b2507e4a9f3c14cb9f3e99cb711bc34d081a4
Mahdi Zaini <73606041+MMZaini@users.noreply.github.com>
2021-08-06 15:25:49 +0000

    variable

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-let Clicks = 0
-let ClickPower = 1
-let AmountPowerfulClicks = 0
+let clicks = 0
+let clickPower = 1
+let amountPowerfulClicks = 0
 
 let price1 = 15
 let multiplier1 = 1.45 * price1
 price1 = multiplier1
 
 function text() {
-  Clicks = Clicks + ClickPower + AmountPowerfulClicks
-  let line = "You have " + Clicks.toFixed(1) + " Clicks"
+  clicks = clicks + clickPower + amountPowerfulClicks
+  let line = "You have " + clicks.toFixed(1) + " clicks"
   document.getElementById("clickTime").innerHTML = line
 }
 
 function shopToggle() {
   let x = document.getElementById("shopText");
   if (x.innerHTML === "") {
-    x.innerHTML = "1. Powerful Clicks - Price = " + price1.toFixed(1) + " - Increases purchasing power by 1";
+    x.innerHTML = "1. Powerful clicks - Price = " + price1.toFixed(1) + " - Increases purchasing power by 1";
   } else {
     x.innerHTML = "";
   }
 }
 
 function buy1() {
-  if (Clicks < price1)
-    console.log("User does not have enough Clicks! He has " + Clicks + " Clicks! He needs " + price1.toFixed(1) + " Clicks!")
+  if (clicks < price1)
+    console.log("User does not have enough clicks! He has " + clicks + " clicks! He needs " + price1.toFixed(1) + " clicks!")
 
   else {
-    Clicks = Clicks - price1
-    line = "You have " + Clicks.toFixed(1) + " Clicks"
+    clicks = clicks - price1
+    line = "You have " + clicks.toFixed(1) + " clicks"
     multiplier1 = 1.45 * price1
     price1 = multiplier1
     shopToggle()
     shopToggle()
-    AmountPowerfulClicks = AmountPowerfulClicks + 1
+    amountPowerfulClicks = amountPowerfulClicks + 1
     document.getElementById("clickTime").innerHTML = line
-    document.getElementById("up1").innerHTML = "You have " + AmountPowerfulClicks + " 'Powerful Clicks' Upgrades"
+    document.getElementById("up1").innerHTML = "You have " + amountPowerfulClicks + " 'Powerful clicks' Upgrades"
   }
 }
