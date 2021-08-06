dfa64967312f65ee0316ffb3a85076d729baaf31
Mahdi Zaini <73606041+MMZaini@users.noreply.github.com>
2021-08-06 15:24:09 +0000

    variable changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let Clicks = 0
-let clickamo = 1
+let ClickPower = 1
 let AmountPowerfulClicks = 0
 
 let price1 = 15
@@ -7,7 +7,7 @@ let multiplier1 = 1.45 * price1
 price1 = multiplier1
 
 function text() {
-  Clicks = Clicks + clickamo
+  Clicks = Clicks + ClickPower + AmountPowerfulClicks
   let line = "You have " + Clicks.toFixed(1) + " Clicks"
   document.getElementById("clickTime").innerHTML = line
 }
@@ -33,7 +33,6 @@ function buy1() {
     shopToggle()
     shopToggle()
     AmountPowerfulClicks = AmountPowerfulClicks + 1
-    clickamo = clickamo + 1
     document.getElementById("clickTime").innerHTML = line
     document.getElementById("up1").innerHTML = "You have " + AmountPowerfulClicks + " 'Powerful Clicks' Upgrades"
   }
