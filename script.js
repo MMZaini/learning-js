83162081a093b0f432199b4e1aaefccebf539d61
Mahdi Zaini <73606041+MMZaini@users.noreply.github.com>
2021-08-06 15:13:29 +0000

    variable names

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let clicks = 0
+let Clicks = 0
 let clickamo = 1
-let amo1 = 0
+let AmountPowerfulClicks = 0
 
 let price1 = 15
 let multiplier1 = price1 / 4 + price1
 price1 = multiplier1
 
 function text() {
-  clicks = clicks + clickamo
-  let line = "You have " + clicks.toFixed(1) + " clicks"
+  Clicks = Clicks + clickamo
+  let line = "You have " + Clicks.toFixed(1) + " Clicks"
   document.getElementById("clickTime").innerHTML = line
 }
 
@@ -22,19 +22,19 @@ function shopToggle() {
 }
 
 function buy1() {
-  if (clicks < price1)
-    console.log("User does not have enough clicks! He has " + clicks + " clicks! He needs " + price1.toFixed(1) + " clicks!")
+  if (Clicks < price1)
+    console.log("User does not have enough Clicks! He has " + Clicks + " Clicks! He needs " + price1.toFixed(1) + " Clicks!")
 
   else {
-    clicks = clicks - price1
-    line = "You have " + clicks.toFixed(1) + " clicks"
+    Clicks = Clicks - price1
+    line = "You have " + Clicks.toFixed(1) + " Clicks"
     multiplier1 = 1.25 * price1
     price1 = multiplier1
     shopToggle()
     shopToggle()
-    amo1 = amo1 + 1
+    AmountPowerfulClicks = AmountPowerfulClicks + 1
     clickamo = clickamo + 1
     document.getElementById("clickTime").innerHTML = line
-    document.getElementById("up1").innerHTML = "You have " + amo1 + " 'Powerful Clicks' Upgrades"
+    document.getElementById("up1").innerHTML = "You have " + AmountPowerfulClicks + " 'Powerful Clicks' Upgrades"
   }
 }
